eedecdf111db223325096f5659d6ad7b65d4090c
J. Zhao <rho.ajax@gmail.com>
2018-04-14 23:23:01 +0100

    clean up

--- newt_archive/rpc-base/newt.thrift
+++
@@ -1,8 +0,0 @@
-typedef i32 response  /* user provide */
-typedef i32 request /* user provide */
-
-service Predict {
-    response add(1:request req)
-}
-
-
--- newt_archive/rpc-base/splus.ml
+++
@@ -1,2 +0,0 @@
-let plus1 a b = a + (int_of_string) b
-let plus a = a + 1
